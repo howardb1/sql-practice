2635c9e323be9bee001c96ff7fc7f6d7156d313b
howardb1 <howardb1@wit.edu>
2025-07-31 15:21:27 -0400

    confirmation rate solution

---
+++ confirmation_rate.sql
@@ -0,0 +1,4 @@
+SELECT s.user_id, COALESCE( ROUND( AVG(CASE WHEN  action = 'confirmed' THEN 1 ELSE 0 END ), 2), 0) AS confirmation_rate 
+FROM Signups s
+LEFT JOIN Confirmations ON s.user_id = Confirmations.user_id
+GROUP BY s.user_id;
